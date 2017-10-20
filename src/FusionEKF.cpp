cab19645a8f131111c63e965ebf7368b064bcdef
ahmadesh <ahmade@uw.edu>
2017-10-19 21:55:41 -0700

    preparing for submission

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -156,10 +156,10 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     // Laser updates
     ekf_.R_ = R_laser_;
     ekf_.H_ = H_laser_;
-    ekf_.Update(measurement_pack.raw_measurements_);
+    //ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
-  //cout << "x_ = " << ekf_.x_ << endl;
-  //cout << "P_ = " << ekf_.P_ << endl;
+  cout << "x_ = " << ekf_.x_ << endl;
+  cout << "P_ = " << ekf_.P_ << endl;
 }
